5e3ec78e7a9f98365368051140b87304e5a73603
C E, Arjun <Arjun.CE@Clarivate.com>
2021-07-30 16:05:36 +0530

    login page complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularMaterialModule } from './shared/angular-material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FormsModule, ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import Typewriter from 't-writer.js';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  hide = true;
+  loginForm: FormGroup;
+  constructor(public fb: FormBuilder) {
+    this.createLoginForm();
+   }
+
+  ngOnInit(): void {
+    this.createLoginForm();
+    const target = document.querySelector('.my-title')
+    const writer = new Typewriter(target, {
+      loop: false,
+      typeSpeed: 80,
+      deleteSpeed: 80,
+      typeColor: 'white',
+      cursorColor:'white'
+    })
+    
+    writer
+      .type('Hi, Welcome to Keep Me Conectted')
+      .rest(500)
+      .changeOps({ deleteSpeed: 80 })
+      .remove(6)
+      .type('nected. Explore people with similar interests')
+      .rest(500)
+      .type(' and take part in various activities.')
+      .start()
+
+  }
+
+  createLoginForm() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-z]{2,4}$")]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(){
+    let crendentials = this.loginForm.value;
+    this.loginForm.reset();
+  }
+}
